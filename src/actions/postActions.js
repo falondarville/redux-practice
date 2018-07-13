79727e7817509ccc07e10a4b1dcace9cc5be6e9c
Falon Darville <falondarville@gmail.com>
2018-07-13 15:03:04 -0700

    add state mapping

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -3,7 +3,7 @@ import { FETCH_POSTS, NEW_POST } from './types';
 export const fetchPosts = () => dispatch => {
 	fetch('https://jsonplaceholder.typicode.com/posts')
 	.then(res => res.json())
-	.then(post => dispatch({
+	.then(posts => dispatch({
 		type: FETCH_POSTS,
 		payload: posts
 	}));
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
 
-export default class Posts extends Component {
+class Posts extends Component {
+
+	componentDidMount(){
+		this.props.fetchPosts();
+	}
 
 	render(){
-
-		const postItems = this.state.posts.map(post => <div key={post.id}>
+		const postItems = this.props.posts.map(post => <div key={post.id}>
 				<h3>{post.title}</h3>
 				<p>{post.body}</p>
 			</div>);
@@ -17,3 +22,9 @@ export default class Posts extends Component {
 			)
 	}
 }
+
+const mapStateToProps = state => ({
+	posts: state.posts.items
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
